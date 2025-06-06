44b9a5403ec0b576f4cecf30ebd902006d0ce30b
hhnqqq <895228612@qq.com>
2025-06-06 02:41:30 +0000

    model/nt/train_llama_with_nt.py is added to define the new structure of llama+nt

---
+++ model/nt/train_llama_with_nt.py
@@ -0,0 +1,58 @@
+import torch.nn as nn
+
+from typing import Optional
+
+from common.registry import registry
+from model.projector import get_multimodal_projector
+from model.llama.model import precompute_freqs_cis, Transformer
+from model.nt.nt_model import NtModel
+from model import BaseTokenizer, NtTokenizer, Llama3Tokenizer
+
+
+def initialize_transformer(model):
+    for p in model.parameters():
+        if p.dim() > 1:
+            nn.init.xavier_uniform_(p)
+    return model
+
+@registry.register_model(["llama1_with_nt", "llama2_with_nt"])
+class LlamaWithNt(nn.Module):
+    def __init__(self, config, tokenizer:Optional[str]=None, multimodal_tokenizer:Optional[str]=None):
+        super().__init__()
+        self.config = config
+        nt_config = config.multimodal_model_config
+        self.model = Transformer(config)
+        self.multimodal_model = NtModel(nt_config)
+        self.multimodal_projector = get_multimodal_projector(config)
+        initialize_transformer(self.multimodal_projector)
+        try:
+            if tokenizer is None:
+                self.tokenizer = BaseTokenizer(config.tokenizer)
+            else:
+                self.tokenizer = BaseTokenizer(tokenizer)
+            if multimodal_tokenizer is None:
+                self.multimodal_tokenizer = NtTokenizer(registry.get_path("tokenizer_nt"))
+            else:
+                self.multimodal_tokenizer = NtTokenizer(multimodal_tokenizer)
+        except:
+            pass
+        self.freqs_cis = precompute_freqs_cis(
+            config.dim // config.n_heads, 
+            config.max_seq_len * 2,
+            config.rope_theta
+        )
+
+    def forward(self):
+        pass
+
+@registry.register_model("llama3_with_nt")
+class Llama3WithNt(LlamaWithNt):
+    def __init__(self, config, tokenizer:Optional[str]=None):
+        super().__init__(config)
+        try:
+            if tokenizer is None:
+                self.tokenizer = Llama3Tokenizer(config.tokenizer)
+            else:
+                self.tokenizer = Llama3Tokenizer(tokenizer)
+        except:
+            pass
